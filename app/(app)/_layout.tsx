e3d9116c1d94bc032999e498794a0743e954baa1
roy-sandoval <roy@roysandoval.com>
2023-12-19 22:02:08 -0700

    Generate a ZeroDev account with private key

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,33 +1,37 @@
 import { LocalAccountSigner } from '@alchemy/aa-core';
-import { ZeroDevConnector } from '@zerodev/wagmi';
+import { ECDSAProvider } from '@zerodev/sdk';
 import { Redirect, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useConnect } from 'wagmi';
+import { useEffect, useState } from 'react';
 
 import { useSession } from '../../store/auth-context';
-import { chains } from '../_layout';
 
 export default function Layout() {
+  const [address, setAddress] = useState('');
+  const [loading, setLoading] = useState(false);
   const { token } = useSession();
-  const { connect } = useConnect();
 
   useEffect(() => {
     if (token) {
-      try {
-        connect({
-          connector: new ZeroDevConnector({
-            chains,
-            options: {
-              projectId: process.env.EXPO_PUBLIC_ZERODEV_ID,
-              owner: LocalAccountSigner.privateKeyToAccountSigner(token as `0x${string}`),
-            },
-          }),
-        });
-      } catch (e) {
-        console.log(e);
-      }
+      (async () => {
+        try {
+          setLoading(true);
+          const ecdsaProvider = await ECDSAProvider.init({
+            projectId: process.env.EXPO_PUBLIC_ZERODEV_ID || '',
+            owner: LocalAccountSigner.privateKeyToAccountSigner(token as `0x${string}`),
+          });
+          setAddress(await ecdsaProvider.getAddress());
+          setLoading(false);
+        } catch (e) {
+          console.log(e);
+        }
+      })();
     }
   }, [token]);
+
+  useEffect(() => {
+    console.log('Address: ', address);
+  }, [address]);
+
   if (!token) {
     return <Redirect href="/login" />;
   }
