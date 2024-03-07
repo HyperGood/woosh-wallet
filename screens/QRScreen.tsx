09a2e766109d812ce0c9f6b4807cabf0da3fec7a
Jonathan Ocegueda <jonathan3492@hotmail.com>
2024-03-06 19:21:51 -0700

    feat: Scan QR code capability w errors

--- screens/QRScreen.tsx
+++ screens/QRScreen.tsx
@@ -3,92 +3,176 @@ import { Link, router } from 'expo-router';
 import { Feather } from '@expo/vector-icons';
 import { COLORS } from '../constants/global-styles';
 import i18n from '../constants/i18n';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../components/UI/Button';
-import { scale } from '../utils/scalingFunctions';
 import QRIcon from '../assets/images/icons/QRIcon';
 
+import { CameraView, useCameraPermissions } from 'expo-camera/next';
+import * as Clipboard from 'expo-clipboard';
+
 const QRScreen = () => {
-    function createWallet() {
-      console.log('Creating wallet Wallet');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (showAlert) {
+      timer = setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
     }
-  
-    return (
+    return () => clearTimeout(timer);
+  }, [showAlert]);
+
+  if(!permission){
     <View style={styles.container}>
-      <View
-        style={{
-          alignSelf: 'flex-start',
-          paddingTop: 56,
-          paddingLeft: 16,
-        }}>
-        <Pressable style={styles.backButton} onPress={() => router.push('/')}>
-          <Feather name="arrow-left" size={24} color={COLORS.dark} style={{ opacity: 0.5 }} />
-        </Pressable>
-      </View>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingBottom: 100}}>
-        <View style={styles.qRPlaceHolder}><QRIcon color={COLORS.dark} /></View>
-        <Text style={styles.accountName}>$account1</Text>
-        {true && (
-          <View>
-            <Text style={{ fontSize: 17, fontFamily: 'Satoshi', textAlign: 'center' }}>Requesting</Text>
-            <Text style={styles.number}>
-              $500.
-              <Text style={styles.decimal}>00 MXN</Text>
-            </Text>
-            <Text style={{ fontSize: 17, fontFamily: 'Satoshi', opacity: .5, textAlign: 'center' }}>26.79 USDc</Text>
-          </View>
-        )}
-        <View style={styles.buttonContainer}>
-          <Link href="/" asChild>
-            <Button title="Edit Amount" type="primary" onPress={(createWallet)} />
-          </Link>
-        </View>
-      </View>
+      <Text>No tienes permisos para acceder a la cámara.</Text>
+      <Button title="Solicitar permisos" onPress={requestPermission} />
     </View>
+  }
+
+  if (!permission?.granted) {
+    <View style={styles.container}>
+      <Text>No tienes permisos para acceder a la cámara.</Text>
+      <Button title="Solicitar permisos" onPress={requestPermission} />
+    </View>
+  }
+
+  
+
+  const handleBarcodeScanned = async ({ type="qr", data }: { type: string, data: string }) => {
+    setShowAlert(true);
+    await Clipboard.setStringAsync(data);
+  };
+
+  function createWallet() {
+    console.log('Creating wallet Wallet');
+  }
+  
+  return (
+    <View style={styles.container}>
+      <CameraView
+        barcodeScannerSettings={{
+          barcodeTypes: ['qr'],
+        }}
+        style={styles.camera}
+        facing={'back'}
+        onBarcodeScanned={handleBarcodeScanned}
+      />
+      {showAlert && (
+        <View style={styles.alertContainer}>
+          <Text style={styles.alertText}>Código copiado al portapapeles</Text>
+        </View>
+      )}
+    </View>
+    // <View style={styles.container}>
+    //   <View
+    //     style={{
+    //       alignSelf: 'flex-start',
+    //       paddingTop: 56,
+    //       paddingLeft: 16,
+    //     }}>
+    //     <Pressable style={styles.backButton} onPress={() => router.push('/')}>
+    //       <Feather name="arrow-left" size={24} color={COLORS.dark} style={{ opacity: 0.5 }} />
+    //     </Pressable>
+    //   </View>
+    //   <View
+    //     style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingBottom: 100 }}>
+    //     <View style={styles.qRPlaceHolder}>
+    //       <QRIcon color={COLORS.dark} />
+    //     </View>
+    //     <Text style={styles.accountName}>$account1</Text>
+    //     {true && (
+    //       <View>
+    //         <Text style={{ fontSize: 17, fontFamily: 'Satoshi', textAlign: 'center' }}>
+    //           Requesting
+    //         </Text>
+    //         <Text style={styles.number}>
+    //           $500.
+    //           <Text style={styles.decimal}>00 MXN</Text>
+    //         </Text>
+    //         <Text
+    //           style={{ fontSize: 17, fontFamily: 'Satoshi', opacity: 0.5, textAlign: 'center' }}>
+    //           26.79 USDc
+    //         </Text>
+    //       </View>
+    //     )}
+    //     <View style={styles.buttonContainer}>
+    //       <Link href="/" asChild>
+    //         <Button title="Edit Amount" type="primary" onPress={createWallet} />
+    //       </Link>
+    //     </View>
+    //   </View>
+    // </View>
   );
 };
 export default QRScreen;
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: COLORS.primary[400],
-  },
-  backButton: {
-    backgroundColor: COLORS.light,
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  qRPlaceHolder: {
-    height: 257,
-    width: 257,
-    backgroundColor: COLORS.light,
-    borderRadius: 16,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  accountName: {
-    fontSize: 32,
-    fontFamily: 'Satoshi',
-    fontWeight: "700",
-    color: COLORS.dark,
-    marginVertical: 20,
+  camera: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  number: {
-    fontSize: 64,
-    fontFamily: 'FHOscar',
-    marginVertical: 1,
+  alertContainer: {
+    backgroundColor: '#FFEA9F',
+    position: 'absolute',
+    width: 270,
+    height: 52,
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  alertText: {
+    fontSize: 17,
     textAlign: 'center',
-  },
-  decimal: {
-    fontSize: 32,
-    fontFamily: 'FHOscar',
-  },
-  buttonContainer: {
-    width: 224,
-    marginTop: 24,
-    flexDirection: 'row',
+    fontFamily: 'Satoshi',
   }
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: COLORS.primary[400],
+  // },
+  // backButton: {
+  //   backgroundColor: COLORS.light,
+  //   width: 48,
+  //   height: 48,
+  //   borderRadius: 24,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
+  // qRPlaceHolder: {
+  //   height: 257,
+  //   width: 257,
+  //   backgroundColor: COLORS.light,
+  //   borderRadius: 16,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // accountName: {
+  //   fontSize: 32,
+  //   fontFamily: 'Satoshi',
+  //   fontWeight: "700",
+  //   color: COLORS.dark,
+  //   marginVertical: 20,
+  // },
+  // number: {
+  //   fontSize: 64,
+  //   fontFamily: 'FHOscar',
+  //   marginVertical: 1,
+  //   textAlign: 'center',
+  // },
+  // decimal: {
+  //   fontSize: 32,
+  //   fontFamily: 'FHOscar',
+  // },
+  // buttonContainer: {
+  //   width: 224,
+  //   marginTop: 24,
+  //   flexDirection: 'row',
+  // }
 });
